cbfbb1c7cf4f2a5e4ea18646e3688520fa39aeed
Victor <vrmm1998@gmail.com>
2024-02-26 15:17:24 -0500

    add axios to get api data and show pokemon list

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,8 +1,13 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getPokemons } from './store/slices/pokemon';
 
 export const PokemonApp = () => {
+  const {
+    isLoading,
+    page,
+    pokemons = [],
+  } = useSelector((state) => state.pokemons);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -13,9 +18,16 @@ export const PokemonApp = () => {
     <>
       <h1>Pokemon App</h1>
       <hr />
+      <span>Cargando: {isLoading ? 'SI' : 'NO'}</span>
       <ul>
-        <li>Hola nsjs</li>
+        {pokemons.map(({ name }) => (
+          <li key={name}>{name}</li>
+        ))}
       </ul>
+      <p>Pagina {page}</p>
+      <button disabled={isLoading} onClick={() => dispatch(getPokemons(page))}>
+        Siguiente
+      </button>
     </>
   );
 };
--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,13 +1,18 @@
+import { pokemonApi } from '../../../api/pokemonApi';
 import { setPokemons, startLoadingPokemons } from './pokemonSlice';
 
 export const getPokemons = (page = 0) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
 
-    // Realizar peticion http
-    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
-    const data = await resp.json();
+    // // Realizar peticion http usando Fetch Api
+    // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
+    // const data = await resp.json();
 
-    dispatch(setPokemons({pokemons: data.results, page: page + 1}));
+    // Realizar peticion http usando Axios
+    const resp = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
+    console.log(resp);
+
+    dispatch(setPokemons({ pokemons: resp.data.results, page: page + 1 }));
   };
 };
